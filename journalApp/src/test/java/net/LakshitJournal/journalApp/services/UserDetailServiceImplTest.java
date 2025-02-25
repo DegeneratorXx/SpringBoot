cbddf0d2dfedc746ad52356d26a839b44cce04de
Your GitHub Username <bt22eci027@iiitn.ac.in>
2025-02-25 17:41:40 +0530

    mockito and junit testing

---
+++ journalApp/src/test/java/net/LakshitJournal/journalApp/services/UserDetailServiceImplTest.java
@@ -0,0 +1,40 @@
+package net.LakshitJournal.journalApp.services;
+
+import net.LakshitJournal.journalApp.Repository.UserRepo;
+import net.LakshitJournal.journalApp.Services.UserDetailServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+
+import static org.mockito.Mockito.when;
+
+public class UserDetailServiceImplTest {
+
+    @InjectMocks
+    private UserDetailServiceImpl userDetailService;
+
+    @MockBean
+    private UserRepo userRepo;
+
+    @BeforeEach
+    void setUp(){
+        MockitoAnnotations.initMocks(this);
+    }
+    @Test
+    public void loadUserByUsernameTest(){
+        when(userRepo.findByUserName(ArgumentMatchers.anyString()))
+                .thenReturn((net.LakshitJournal.journalApp.entity.User)User.builder()
+                        .username("ram")
+                        .password("inrinrick")
+                        .roles(new String[0]) // Empty array instead of ArrayList
+                        .build());
+        UserDetails user = userDetailService.loadUserByUsername("ram");
+    }
+}
---
+++ journalApp/src/test/java/net/LakshitJournal/journalApp/services/UserServiceTest.java
@@ -0,0 +1,34 @@
+package net.LakshitJournal.journalApp.services;
+
+import net.LakshitJournal.journalApp.Repository.UserRepo;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+public class UserServiceTest {
+
+    @Autowired
+    private UserRepo userRepo;
+
+    @Disabled
+    @Test
+    public void testfindByUserName(){
+        assertNotNull(userRepo.findByUserName("Lakshit"));
+    }
+
+
+    @ParameterizedTest
+    @CsvSource({
+            "Lakshit",
+            "ADMIN"
+
+    })
+    public void testfindByUserName2(String userName){
+        assertNotNull(userRepo.findByUserName(userName));
+    }
+}
